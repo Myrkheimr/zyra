207acdb20cd2126e705d5b5e9be98087aae658bd
maranix <ramanverma4183@gmail.com>
2025-02-24 10:54:51 +0530

    test(HBA): write basic suite of tests

--- src/test.zig
+++ src/test.zig
@@ -1,5 +1,6 @@
-pub const App = @import("App.zig");
-pub const argument = @import("argument.zig");
+// pub const App = @import("App.zig");
+// pub const argument = @import("argument.zig");
+pub const HashBackedArray = @import("hash_backed_array.zig");
 
 test {
     @import("std").testing.refAllDecls(@This());
